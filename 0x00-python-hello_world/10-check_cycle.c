52f6776456f20f038b90906828514f4c796514bf
shahdwaleed <shahdwaleed@gmail.com>
2024-05-03 05:58:00 -0700

    c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/*
+ * check_cycle - check if there is any cycle in the list
+ * @*list: the head of the list we are checking
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	const listint_t *current;
+	current = list;
+	while (current != NULL)
+    {
+        current = current->next;
+	if( current == list)
+	{
+		return(1);
+	}
+    }
+	return (0);
+}
