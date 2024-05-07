14b32f00d2583dd79dd224330f283981aac4be0a
shahdwaleed <shahdwaleed@gmail.com>
2024-05-07 07:43:56 -0700

    c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -14,15 +14,15 @@ int check_cycle(listint_t *list)
 
         listint_t *slow, *fast;	
 
-	current = list;
+	slow = list;
+        fast = list->next;
 
 	if (!list)
 		return (0);
-        slow = list;
-	fast = list->next;
+    
 	while (fast && slow && fast->next)
 	{
-		if (slow == fast)
+		if (slow == fast || slow->next == list)
 		{
 			return (1);
 		}
